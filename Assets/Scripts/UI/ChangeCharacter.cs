d72694f689d6afb341c8cc9c60b10c837502880f
Sergio López Ceballos <sergio.lceballos@alumnos.upm.es>
2019-03-20 18:00:55 +0100

    Switch to other character

---
+++ Assets/Scripts/UI/ChangeCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCharacter : MonoBehaviour
+{
+    public GameObject panda;
+    public GameObject kero;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+    }
+
+    public void switchToPanda()
+    {
+        panda.SetActive(true);
+        kero.SetActive(false);
+    }
+
+    public void switchToKero()
+    {
+        panda.SetActive(false);
+        kero.SetActive(true);
+    }
+}
