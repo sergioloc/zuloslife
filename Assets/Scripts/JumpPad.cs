c6d6e39a8596f44bd69173b01f02218fbf7bfd75
Sergio López Ceballos <sergio.lceballos@alumnos.upm.es>
2019-08-08 19:21:31 +0200

    Jump Pad

---
+++ Assets/Scripts/JumpPad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+
+    public float jumpForce = 11f;
+    public ParticleSystem electricPulse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpForce = jumpForce * 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+        electricPulse.Play();
+    }
+}
