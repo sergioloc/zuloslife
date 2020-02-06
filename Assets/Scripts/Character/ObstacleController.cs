9c03e1016ce84d1f7700b0636fecbbc9bcefed91
Sergio López Ceballos <sergioloc2@gmail.com>
2020-02-06 19:02:18 +0100

    Create Chapter 2

---
+++ Assets/Scripts/Character/ObstacleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public int damage;
+    public float speed;
+    public GameObject particle;
+
+    void Start()
+    {
+        Destroy(gameObject, 4f);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<TricycleController>().health -= damage;
+            Debug.Log(collision.GetComponent<TricycleController>().health);
+            Instantiate(particle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Character/TricycleController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TricycleController : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float incrementor;
+    public float speed;
+    private float maxHeight;
+    private float minHeight;
+    public int health = 3;
+    public GameObject particle;
+    public Animator camAnim;
+    public bool shake = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHeight = incrementor;
+        minHeight = -incrementor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            Instantiate(particle, transform.position, Quaternion.identity);
+            if (shake) camAnim.SetTrigger("Shake2");
+            targetPos = new Vector2(transform.position.x, transform.position.y + incrementor);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight) 
+        {
+            Instantiate(particle, transform.position, Quaternion.identity);
+            if (shake) camAnim.SetTrigger("Shake2");
+            targetPos = new Vector2(transform.position.x, transform.position.y - incrementor);
+        }
+    }
+}
