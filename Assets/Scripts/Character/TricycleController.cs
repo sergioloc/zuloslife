9763ba3eebe107b23a94781b832d0f77f1f82bc1
Sergio López Ceballos <sergioloc2@gmail.com>
2020-02-12 22:21:42 +0100

    Kekeo's animations

--- Assets/Scripts/Character/TricycleController.cs
+++ Assets/Scripts/Character/TricycleController.cs
@@ -10,15 +10,18 @@ public class TricycleController : MonoBehaviour
     public int health = 3;
     public GameObject particle;
     public Animator camAnim;
+    private Animator kekeoAnim;
     public bool shake = true;
     private float maxHeight;
     private float minHeight;
+    public GameObject wheelF, wheelB1, wheelB2;
 
 
     void Start()
     {
         maxHeight = range;
         minHeight = -range;
+        kekeoAnim = gameObject.GetComponent<Animator>();
     }
 
     void Update()
@@ -37,22 +40,29 @@ public class TricycleController : MonoBehaviour
         {
             MoveDown();
         }
+
+        //Rotate wheels
+        wheelF.transform.Rotate(0, 0, -180 * Time.deltaTime);
+        wheelB1.transform.Rotate(0, 0, -180 * Time.deltaTime);
+        wheelB2.transform.Rotate(0, 0, -180 * Time.deltaTime);
     }
 
     public void MoveUp()
     {
         if (transform.position.y < maxHeight)
         {
+            kekeoAnim.SetTrigger("Jump");
             transform.position = new Vector2(transform.position.x, transform.position.y + range);
             if (shake) camAnim.SetTrigger("Shake2");
             Instantiate(particle, transform.position, Quaternion.identity);
-        }   
+        }
     }
 
     public void MoveDown()
     {
         if (transform.position.y > minHeight)
         {
+            kekeoAnim.SetTrigger("Jump");
             transform.position = new Vector2(transform.position.x, transform.position.y - range);
             //transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y - range), speed * Time.deltaTime);
             if (shake) camAnim.SetTrigger("Shake2");
