6fddf147a7f492af8b9671e3c4ac1808224ac1da
Sergio López Ceballos <sergioloc2@gmail.com>
2020-05-11 14:36:31 +0200

    New water texture animation

---
+++ Assets/Scripts/Level1/CameraIgnore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraIgnore : MonoBehaviour
+{
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.cullingMask = 1 << 13 | 1 << 9;
+    }
+}
