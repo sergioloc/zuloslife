b1ae9ffd33236c24d8bcf987d69a7ac3fc021e32
Sergio López Ceballos <sergioloc2@gmail.com>
2020-05-27 22:05:13 +0200

    New level prefab

---
+++ Assets/Scripts/Menu/Level.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Level: MonoBehaviour
+{
+    private int id;
+    public string title;
+    public string intro;
+    public string outro;
+    public bool unlocked;
+    public Button button;
+
+    void Start()
+    {
+        button.onClick.AddListener(() => { GoToLevel(); });
+    }
+
+    private void GoToLevel(){
+        if (unlocked)
+            SceneManager.LoadScene(intro);
+    }
+}
