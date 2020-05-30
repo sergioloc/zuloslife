5ac649cb9545cc79ff77565bfbc11c57f9bcf4b7
Sergio López Ceballos <sergioloc2@gmail.com>
2020-05-30 14:10:55 +0200

    Don't stop music in menu

---
+++ Assets/Scripts/Menu/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    private AudioSource audioSource;
+     private void Awake()
+     {
+         DontDestroyOnLoad(transform.gameObject);
+     }
+ 
+     public void PlayMusic()
+     {
+         audioSource.Play();
+     }
+ 
+     public void StopMusic()
+     {
+         audioSource.Stop();
+     }
+}
